9cc39f1e7c6679aa6739fc35a50ff9ff90f5c984
aparnaratheesh55 <aparnaratheesh2000@gmail.com>
2023-05-22 01:13:02 +0530

    first python file added

---
+++ first.py
@@ -0,0 +1,14 @@
+def simple(p,t,r):
+    si=(p*t*r)/100
+    print("simple interest is",si)
+    return si
+age=int(input("enter age"))
+p=int(input("enter principle amount"))
+t=int(input("enter the number of years"))
+
+if(age>70):
+    r=12
+if(age<70):
+    r=10
+simple(p,t,r)
+        
