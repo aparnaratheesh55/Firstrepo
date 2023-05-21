47dfac32a6d8209c2b0a3d67ff6120928cb1db78
aparnaratheesh55 <aparnaratheesh2000@gmail.com>
2023-05-22 01:27:55 +0530

    new commit

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ def simple(p,t,r):
     si=(p*t*r)/100
     print("simple interest is",si)
     return si
-age=int(input("enter age"))
+age=int(input("enter age please"))
 p=int(input("enter principle amount"))
 t=int(input("enter the number of years"))
 
